8c652932009b4f9cf8c2ef36aa4ec4ba99dad807
Krishan Shamod <krishanshamod@gmail.com>
2022-05-08 16:10:51 +0530

    Added the first fragment save data functionality

--- app/src/main/java/com/krishanshamod/simple_android_application_5/FirstFragment.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_5/FirstFragment.kt
@@ -15,6 +15,11 @@ class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
+    private var userEmail: String = ""
+    private var userFirstName: String = ""
+    private var userLastName: String = ""
+    private var userAge: String = ""
+
     var db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
@@ -29,6 +34,31 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.submitButton.setOnClickListener {
+            binding.apply {
+
+                // Get the input from the user
+                userEmail = email.text.toString()
+                userFirstName = firstName.text.toString()
+                userLastName = lastName.text.toString()
+                userAge = age.text.toString()
+
+                if (userEmail.isEmpty() || userFirstName.isEmpty() || userLastName.isEmpty() || userAge.isEmpty()) {
+                    // If the user did not enter anything, show an error message
+                    errorView.setText("Please fill all details")
+                } else {
+
+                    // save the user details in the database
+                    db.collection("users").document(userEmail).set(mapOf(
+                        "email" to userEmail,
+                        "firstName" to userFirstName,
+                        "lastName" to userLastName,
+                        "age" to userAge
+                    ))
+                }
+            }
+        }
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
