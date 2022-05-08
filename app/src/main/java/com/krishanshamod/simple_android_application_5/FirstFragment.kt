3da164f13344737eb56e143ce7a4632072acc230
Krishan Shamod <krishanshamod@gmail.com>
2022-05-08 15:58:31 +0530

    Updated the first fragment layout

--- app/src/main/java/com/krishanshamod/simple_android_application_5/FirstFragment.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_5/FirstFragment.kt
@@ -1,23 +1,21 @@
 package com.krishanshamod.simple_android_application_5
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.FirebaseFirestore
 import com.krishanshamod.simple_android_application_5.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    var db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
